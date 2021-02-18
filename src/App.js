ca2fb8c0a7209daa7fdb9027f83b2df1b6e7ecc3
Manish Trivedi <manishtrivedi994@gmail.com>
2021-02-18 14:46:35 +0530

    adding and using Radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -50,11 +51,16 @@ class App extends Component {
   }
     render() {
       const style = {
-        backgroundColor: 'white',
+        backgroundColor: 'green',
+        color: 'white',
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+        }
       };
 
       let persons = null;
@@ -68,15 +74,30 @@ class App extends Component {
               age={person.age}
               key={person.id}
               changed={(event) => this.nameChangedHandler(event, person.id)}/>
-            })}
+            })
+            }
           </div>
-          
         );
+        style.backgroundColor= 'red';
+        style[':hover']= {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }
       }
+
+     let classes = []; 
+     if(this.state.persons.length<=2) {
+       classes.push('red'); //classes = ['red'];
+     }
+     if(this.state.persons.length<=1) {
+       classes.push('bold'); //classes = ['red', 'bold'];
+
+     }
 
     return (
       <div className="App">
         <h1>Hi! I'm React App.</h1>
+        <p className={classes.join(' ')}>This is really working.</p>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Switch Name</button>
@@ -85,5 +106,5 @@ class App extends Component {
     );
     }
   }
-export default App;
+export default Radium(App);
 
